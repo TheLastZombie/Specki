d6c92233993a7ff96c13f9efc3819eceb26f546d
TheLastZombie <eric.roesch.pfr@gmail.com>
2018-07-30 22:18:15 +0200

    Made $name owner-only

--- ich_iel.js
+++ ich_iel.js
@@ -596,18 +596,22 @@ client.on("message", async message => {
 			};
 		};
 		if (command === "name") {
-			if (args && args != "") {
-				console.log(`Ändere Bot-Name zu "${args.join(" ")}".`);
-				client.user.setUsername(args.join(" "));
+			if (message.author.id != 175877241517899776) {
+				message.react("❎");
 			} else {
-				message.channel.fetchMessages({
-					limit: 2
-				}).then(temp => {
-					console.log(`Ändere Bot-Name zu "${temp.last().content}".`);
-					client.user.setUsername(temp.last().content);
-				});
+				if (args && args != "") {
+					console.log(`Ändere Bot-Name zu "${args.join(" ")}".`);
+					client.user.setUsername(args.join(" "));
+				} else {
+					message.channel.fetchMessages({
+						limit: 2
+					}).then(temp => {
+						console.log(`Ändere Bot-Name zu "${temp.last().content}".`);
+						client.user.setUsername(temp.last().content);
+					});
+				};
+				message.react("✅");
 			};
-			message.react("✅");
 		};
 		if (command === "nick") {
 			if (args && args != "") {
