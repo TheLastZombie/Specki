ceae17107b6618e87b62542ac4d09b38da6d40a6
TheLastZombie <eric.roesch.pfr@gmail.com>
2018-07-30 09:38:31 +0200

    Small improvements to $farbe command
    - Check if input is valid
    - Only return name where applicable

--- ich_iel.js
+++ ich_iel.js
@@ -330,30 +330,34 @@ client.on("message", async message => {
 		if (command === "farbe") {
 			if (args && args != "") {
 				var temp = tinycolor(args.join(" "));
-				message.channel.send({
-					embed: {
-						color: parseInt(temp.toHex(), 16),
-						author: {
-							name: "Farbanalyse: " + temp.toHexString().toUpperCase(),
-							icon_url: client.user.avatarURL
-						},
-						url: "https://rsch.neocities.org",
-						fields: [
-							{
-								name: "Werte",
-								value: "Input: " + temp.getOriginalInput() + "\nRGB: " + temp.toRgbString() + "\nHEX: " + temp.toHexString().toUpperCase() + "\nHSV: " + temp.toHsvString() + "\nHSL: " + temp.toHslString() + "\nName: " + temp.toName()
+				if (temp.isValid()) {
+					message.channel.send({
+						embed: {
+							color: parseInt(temp.toHex(), 16),
+							author: {
+								name: "Farbanalyse: " + temp.toHexString().toUpperCase(),
+								icon_url: client.user.avatarURL
 							},
-							{
-								name: "Helligkeit",
-								value: temp.getBrightness() + " / 255 (" + ((temp.isLight()) ? "Hell" : "Dunkel") + ", nach [W3C-Richtlinien](http://www.w3.org/TR/AERT#color-contrast).)"
-							},
-							{
-								name: "Luminanz",
-								value: temp.getLuminance() + " (Nach [W3C-Richtlinien](https://www.w3.org/TR/2008/REC-WCAG20-20081211/#contrast-ratiodef).)"
-							}
-						]
-					}
-				});
+							url: "https://rsch.neocities.org",
+							fields: [
+								{
+									name: "Werte",
+									value: "Input: " + temp.getOriginalInput() + "\nRGB: " + temp.toRgbString() + "\nHEX: " + temp.toHexString().toUpperCase() + "\nHSV: " + temp.toHsvString() + "\nHSL: " + temp.toHslString() + ((temp.toName()) ? "\nName: " + temp.toName())
+								},
+								{
+									name: "Helligkeit",
+									value: temp.getBrightness() + " / 255 (" + ((temp.isLight()) ? "Hell" : "Dunkel") + ", nach [W3C-Richtlinien](http://www.w3.org/TR/AERT#color-contrast).)"
+								},
+								{
+									name: "Luminanz",
+									value: temp.getLuminance() + " (Nach [W3C-Richtlinien](https://www.w3.org/TR/2008/REC-WCAG20-20081211/#contrast-ratiodef).)"
+								}
+							]
+						}
+					});
+				} else {
+					message.react("❎");
+				};
 			} else {
 				message.react("❎");
 			};
