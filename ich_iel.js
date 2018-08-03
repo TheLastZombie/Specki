3b1ac778e8dfbd537b3b2e65a5759b8ba9833ce6
TheLastZombie <eric.roesch.pfr@gmail.com>
2018-08-03 14:40:31 +0200

    Fixed $posten, maybe

--- ich_iel.js
+++ ich_iel.js
@@ -870,49 +870,53 @@ client.on("message", async message => {
 		if (command === "pfosten") {
 			if (args && args != "") {
 				request("https://www.reddit.com/r/" + args.join(" ") + "/random/.json", function (error, response, body) {
-					if (JSON.parse(body)[0]["data"]["children"][0]["data"]["over_18"] == true && message.channel.nsfw == false) {
-						message.react("🔞");
+					if (response.statusCode != 200) {
+						message.channel.send("Error: Requested **https://www.reddit.com/r/" + args.join(" ") + "/random/.json**, got **" + response.statusCode + "**.");
 					} else {
-						message.channel.send({
-							"embed": {
-								"title": JSON.parse(body)[0]["data"]["children"][0]["data"]["title"],
-								"description": JSON.parse(body)[0]["data"]["children"][0]["data"]["selftext"],
-								"fields": [{
-										"name": "Subreddit",
-										"value": JSON.parse(body)[0]["data"]["children"][0]["data"]["subreddit"],
-										"inline": true
-									},
-									{
-										"name": "Autor",
-										"value": JSON.parse(body)[0]["data"]["children"][0]["data"]["author"],
-										"inline": true
-									},
-									{
-										"name": "Datum",
-										"value": new Date(JSON.parse(body)[0]["data"]["children"][0]["data"]["created"] * 1000).toISOString().replace(/T/, " ").replace(/\..+/, ""),
-										"inline": true
-									},
-									{
-										"name": "Votes",
-										"value": "▲ " + JSON.parse(body)[0]["data"]["children"][0]["data"]["ups"] + " | " + JSON.parse(body)[0]["data"]["children"][0]["data"]["downs"] + " ▼",
-										"inline": true
-									},
-									{
-										"name": "Aktivität",
-										"value": "🗩 " + JSON.parse(body)[0]["data"]["children"][0]["data"]["num_comments"] + " | " + JSON.parse(body)[0]["data"]["children"][0]["data"]["num_crossposts"] + " ✕",
-										"inline": true
-									},
-									{
-										"name": "Link",
-										"value": "https://redd.it/" + JSON.parse(body)[0]["data"]["children"][0]["data"]["id"],
-										"inline": true
+						if (JSON.parse(body)[0]["data"]["children"][0]["data"]["over_18"] == true && message.channel.nsfw == false) {
+							message.react("🔞");
+						} else {
+							message.channel.send({
+								"embed": {
+									"title": JSON.parse(body)[0]["data"]["children"][0]["data"]["title"],
+									"description": JSON.parse(body)[0]["data"]["children"][0]["data"]["selftext"],
+									"fields": [{
+											"name": "Subreddit",
+											"value": JSON.parse(body)[0]["data"]["children"][0]["data"]["subreddit"],
+											"inline": true
+										},
+										{
+											"name": "Autor",
+											"value": JSON.parse(body)[0]["data"]["children"][0]["data"]["author"],
+											"inline": true
+										},
+										{
+											"name": "Datum",
+											"value": new Date(JSON.parse(body)[0]["data"]["children"][0]["data"]["created"] * 1000).toISOString().replace(/T/, " ").replace(/\..+/, ""),
+											"inline": true
+										},
+										{
+											"name": "Votes",
+											"value": "▲ " + JSON.parse(body)[0]["data"]["children"][0]["data"]["ups"] + " | " + JSON.parse(body)[0]["data"]["children"][0]["data"]["downs"] + " ▼",
+											"inline": true
+										},
+										{
+											"name": "Aktivität",
+											"value": "🗩 " + JSON.parse(body)[0]["data"]["children"][0]["data"]["num_comments"] + " | " + JSON.parse(body)[0]["data"]["children"][0]["data"]["num_crossposts"] + " ✕",
+											"inline": true
+										},
+										{
+											"name": "Link",
+											"value": "https://redd.it/" + JSON.parse(body)[0]["data"]["children"][0]["data"]["id"],
+											"inline": true
+										}
+									],
+									"image": {
+										"url": JSON.parse(body)[0]["data"]["children"][0]["data"]["url"]
 									}
-								],
-								"image": {
-									"url": JSON.parse(body)[0]["data"]["children"][0]["data"]["url"]
 								}
-							}
-						});
+							});
+						};
 					};
 				});
 			};
