e99546c8faf0980ab4d72dff8fd43dec5b6bfbca
TheLastZombie <eric.roesch.pfr@gmail.com>
2018-09-15 13:26:17 +0200

    thema da loe: du bsit ein dummer hurensohn halt fres

--- ich_iel.js
+++ ich_iel.js
@@ -467,9 +467,9 @@ client.on("message", async message => {
 							}
 						}
 					}).run(args.join(" "));
-					var vmmsg = ((vmout != undefined) ? "Output: \n```" + vmout + "```\n\n" : "") + ((vmlog != "") ? "Console: \n```" + vmlog + "```" : "");
+					var vmmsg = ((vmout != undefined) ? "Output: \n```" + vmout.replace(/(?<!\\)`/g, "\\`") + "```\n\n" : "") + ((vmlog != "") ? "Console: \n```" + vmlog.replace(/(?<!\\)`/g, "\\`") + "```" : "");
 					if (vmmsg) {
-						message.channel.send(vmmsg);
+						message.channel.send(vmmsg.replace(/(?<!\\)`/g, "\\`"));
 					} else {
 						message.channel.send("Nothing returned! ¯\\_(ツ)_/¯");
 					};
@@ -480,7 +480,7 @@ client.on("message", async message => {
 				try {
 					var evout = eval("(" + args.join(" ") + ")");
 					if (evout) {
-						message.channel.send("Output: \n```" + evout + "```\n\nNote: Code is not running in a sandbox and thus console output not accessible.");
+						message.channel.send("Output: \n```" + evout.replace(/(?<!\\)`/g, "\\`") + "```\n\nNote: Code is not running in a sandbox and thus console output not accessible.");
 					};
 				} catch(err) {
 					message.channel.send("Error: \n```" + err.toString() + "```");
