57b7f8b5be8532500ccfee7d6f4308f50efa5bcf
TheLastZombie <eric.roesch.pfr@gmail.com>
2018-10-05 20:31:08 +0200

    error handling deluxe - meiyou

--- ich_iel.js
+++ ich_iel.js
@@ -797,56 +797,60 @@ client.on("message", async message => {
 		};
 		if (command === "movie") {
 			if (args && args != "") {
-				request("https://www.omdbapi.com/?apikey=" + process.env.OMDB_TK + "&s=" + args.join(" "), function (error, response, body) {
-					request("https://www.omdbapi.com/?apikey=" + process.env.OMDB_TK + "&i=" + JSON.parse(body).Search[0].imdbID, function (error, response, body) {
-						var temp = JSON.parse(body);
-						message.channel.send({
-							"embed": {
-								"title": temp.Title + " (" + temp.Year + ")",
-								"description": temp.Plot,
-								"thumbnail": {
-									"url": temp.Poster
-								},
-								"fields": [
-									{
-										"name": "Director",
-										"value": temp.Director,
-										"inline": true
+				try {
+					request("https://www.omdbapi.com/?apikey=" + process.env.OMDB_TK + "&s=" + args.join(" "), function (error, response, body) {
+						request("https://www.omdbapi.com/?apikey=" + process.env.OMDB_TK + "&i=" + JSON.parse(body).Search[0].imdbID, function (error, response, body) {
+							var temp = JSON.parse(body);
+							message.channel.send({
+								"embed": {
+									"title": temp.Title + " (" + temp.Year + ")",
+									"description": temp.Plot,
+									"thumbnail": {
+										"url": temp.Poster
 									},
-									{
-										"name": "Release Date",
-										"value": temp.Released,
-										"inline": true
-									},
-									{
-										"name": "Actors",
-										"value": temp.Actors
-									},
-									{
-										"name": "Genre",
-										"value": temp.Genre,
-										"inline": true
-									},
-									{
-										"name": "Rating",
-										"value": temp.Rated,
-										"inline": true
-									},
-									{
-										"name": "Runtime",
-										"value": temp.Runtime,
-										"inline": true
-									},
-									{
-										"name": "Awards",
-										"value": temp.Awards,
-										"inline": true
-									}
-								]
-							}
+									"fields": [
+										{
+											"name": "Director",
+											"value": temp.Director,
+											"inline": true
+										},
+										{
+											"name": "Release Date",
+											"value": temp.Released,
+											"inline": true
+										},
+										{
+											"name": "Actors",
+											"value": temp.Actors
+										},
+										{
+											"name": "Genre",
+											"value": temp.Genre,
+											"inline": true
+										},
+										{
+											"name": "Rating",
+											"value": temp.Rated,
+											"inline": true
+										},
+										{
+											"name": "Runtime",
+											"value": temp.Runtime,
+											"inline": true
+										},
+										{
+											"name": "Awards",
+											"value": temp.Awards,
+											"inline": true
+										}
+									]
+								}
+							});
 						});
 					});
-				});
+				} catch(err) {
+					message.react("❎");	
+				};
 			} else {
 				message.react("❎");
 			};
