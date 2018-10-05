899f3d6276eb945939bfec9cffba11bcbcacf0c2
TheLastZombie <eric.roesch.pfr@gmail.com>
2018-10-05 20:24:46 +0200

    ich raste aus irgendwann erreich ich mein heroku build limit

--- ich_iel.js
+++ ich_iel.js
@@ -804,7 +804,6 @@ client.on("message", async message => {
 							"embed": {
 								"title": temp.Title + " (" + temp.Year + ")",
 								"description": temp.Plot,
-								"url": (temp.Website ? temp.Website : "https://www.imdb.com/title/" + temp.imdbID),
 								"thumbnail": {
 									"url": temp.Poster
 								},
