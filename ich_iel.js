d133344774e1f0da0059922846b73a206de4f591
TheLastZombie <eric.roesch@web.de>
2018-10-12 11:09:06 +0200

    Update ich_iel.js

--- ich_iel.js
+++ ich_iel.js
@@ -782,12 +782,12 @@ client.on("message", async message => {
 		};
 		if (command === "link") {
 			if (args && args != "") {
-				message.channel.send(args.join(" ").match(/r\/\w+/g).map(i => "https://https://www.reddit.com/" + i).join("\n"));
+				message.channel.send(args.join(" ").match(/r\/\w+/g).map(i => "<https://www.reddit.com/" + i + ">").join("\n"));
 			} else {
 				message.channel.fetchMessages({
 					limit: 2
 				}).then(temp => {
-					message.channel.send(temp.last().content.match(/r\/\w+/g).map(i => "https://https://www.reddit.com/" + i).join("\n"));
+					message.channel.send(temp.last().content.match(/r\/\w+/g).map(i => "<https://www.reddit.com/" + i + ">").join("\n"));
 				});
 			};
 		};
