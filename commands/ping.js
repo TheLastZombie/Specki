a7af8494fc7c21c5238b4507534e8fdc216f364a
TheLastZombie <eric.roesch@web.de>
2018-10-15 10:23:47 +0200

    fuck it

--- commands/ping.js
+++ commands/ping.js
@@ -1,2 +1,1 @@
-var temp = await message.channel.send("Ping...");
-temp.edit("Pong! Latenz: " + (temp.createdTimestamp - message.createdTimestamp) + " ms. API-Latenz: " + Math.round(client.ping) + " ms.");
+message.channel.send("Pong! Latenz: " + Math.round(client.ping) + " ms.");
