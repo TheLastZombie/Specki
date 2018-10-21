394f060c682f0e4dbbefb64023a584af2c3d3801
TheLastZombie <eric.roesch@web.de>
2018-10-21 18:00:10 +0200

    Update name.js

--- commands/name.js
+++ commands/name.js
@@ -1,6 +1,4 @@
 if (ownerIds.includes(message.author.id)) {
-	message.react("❎");
-} else {
 	if (args && args != "") {
 		console.log("Ändere Bot-Name zu \"" + args.join(" ") + "\".");
 		client.user.setUsername(args.join(" "));
@@ -13,4 +11,6 @@ if (ownerIds.includes(message.author.id)) {
 		});
 	};
 	message.react("✅");
+} else {
+	message.react("❎");
 };
