cb81c75ecfc4d27e2363bc0ab6b5aba5d3dde0ee
TheLastZombie <eric.roesch@web.de>
2018-10-23 18:37:10 +0200

    Preparing for 2.1.0

--- commands/about.js
+++ commands/about.js
@@ -39,7 +39,7 @@ message.channel.send({
 		],
 		footer: {
 			icon_url: client.user.avatarURL,
-			text: "v2.0 Pre-Beta | " + client.guilds.size + "G, " + client.channels.size + "C, " + client.users.size + "U | Commit " + commid + " | by @roesch#0611 using discord.js"
+			text: "v2.1.0 | " + client.guilds.size + "G, " + client.channels.size + "C, " + client.users.size + "U | Commit " + commid + " | by @roesch#0611 using discord.js"
 		}
 	}
 });
