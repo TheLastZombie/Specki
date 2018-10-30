0da03c1914791cdf5197845c06af50daf38eb2b4
TheLastZombie <eric.roesch.pfr@gmail.com>
2018-10-30 15:30:41 +0100

    Fix order of images in doubleVision

--- commands/thumb.js
+++ commands/thumb.js
@@ -1,7 +1,7 @@
 if (args.length == 2) {
 	request(args[0]).pipe(fs.createWriteStream(__dirname + "/images/thumb.png")).on("close", function () {
 		request(args[1]).pipe(fs.createWriteStream(__dirname + "/images/full.png")).on("close", function () {
-			exec("doubleVision images/thumb.png images/full.png images/temp.png", function (err, stdout, stderr) {
+			exec("doubleVision images/full.png images/thumb.png images/temp.png", function (err, stdout, stderr) {
 				if (stderr) {
 					message.channel.send("```" + stderr + "```");
 				} else {
