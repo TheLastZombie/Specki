77db79347d53b3843b8ce8cf0995e3dd693ce089
TheLastZombie <eric.roesch.pfr@gmail.com>
2018-11-30 13:06:57 +0100

    Re-add "real" latency to $ping

--- commands/ping.js
+++ commands/ping.js
@@ -1,1 +1,3 @@
-message.channel.send("Pong! Latenz: " + Math.round(client.ping) + " ms.");
+message.channel.send("Ping...").then(function(message) {
+	message.edit("Pong! Latenz: " + (Date.now() - message.createdTimestamp) + " ms. API-Latenz: " + Math.round(client.ping) + " ms.");
+});
