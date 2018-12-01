0e9e51cc598cb7e7edd3cb48e785035ca1e1d5e9
Eric Rösch <eric.roesch.pfr@gmail.com>
2018-12-01 16:53:58 +0100

    Initialize $gb command

---
+++ commands/gb.js
@@ -0,0 +1,22 @@
+var temp = args.shift();
+var endp = ["accessories", "characters", "companies", "concepts", "dlcs", "franchises", "games", "genres", "locations", "objects", "people", "platforms", "rating_boards", "regions", "releases", "videos", "video_categories"];
+if (endp.includes(temp)) {
+    request("https://www.giantbomb.com/api/" + temp + "/?api_key=" + process.env.BOMB_TK + "&format=json&field_list=deck,image,name,site_detail_url&limit=1&filter=name:" + args.join(" "), function (error, response, body) {
+        if (JSON.parse(body).results.length > 0) {
+            message.channel.send({
+                embed: {
+                    title: JSON.parse(body).results[0].name,
+                    description: JSON.parse(body).results[0].deck,
+                    url: JSON.parse(body).results[0].site_detail_url,
+                    thumbnail: {
+                        url: JSON.parse(body).results[0].image.original_url
+                    }
+                }
+            });
+        } else {
+            message.react("❎");
+        };
+    });
+} else {
+    message.channel.send("**" + temp + "** is not a valid endpoint! Supported are: **" + endp.join("**, **") + "**.");
+};
