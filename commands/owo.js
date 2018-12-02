214eb8062b59810d8ec57dd1509c680ed2056961
Eric Rösch <eric.roesch.pfr@gmail.com>
2018-12-02 13:29:03 +0100

    Initialize $owo command

---
+++ commands/owo.js
@@ -0,0 +1,9 @@
+if (args && args != "") {
+	message.channel.send(args.join(" ").replace(/(?:r|l)/g, "w").replace(/(?:R|L)/g, "W").replace(/n([aeiou])/g, 'ny$1').replace(/N([aeiou])/g, 'Ny$1').replace(/N([AEIOU])/g, 'Ny$1').replace(/ove/g, "uv").replace(/\!+/g, " " + ["(・`ω´・)", ";;w;;", "owo", "UwU", ">w<", "^w^"][Math.floor(Math.random() * 6)] + " "));
+} else {
+	message.channel.fetchMessages({
+		limit: 2
+	}).then(temp => {
+		message.channel.send(temp.last().content.replace(/(?:r|l)/g, "w").replace(/(?:R|L)/g, "W").replace(/n([aeiou])/g, 'ny$1').replace(/N([aeiou])/g, 'Ny$1').replace(/N([AEIOU])/g, 'Ny$1').replace(/ove/g, "uv").replace(/\!+/g, " " + ["(・`ω´・)", ";;w;;", "owo", "UwU", ">w<", "^w^"][Math.floor(Math.random() * 6)] + " "));
+	});
+};
