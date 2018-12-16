a5a66152d75f9d2ecc72f3dbbcfcc2c43d830546
TheLastZombie <eric.roesch.pfr@gmail.com>
2018-12-16 18:17:42 +0100

    Update about.js

--- commands/about.js
+++ commands/about.js
@@ -39,7 +39,7 @@ message.channel.send({
 		],
 		footer: {
 			icon_url: client.user.avatarURL,
-			text: "v2.3.2 | " + client.guilds.size + "G, " + client.channels.size + "C, " + client.users.size + "U | Commit " + commid + " | by @rx#0611 using discord.js"
+			text: "v2.3.3 | " + client.guilds.size + "G, " + client.channels.size + "C, " + client.users.size + "U | Commit " + commid + " | by @rx#0611 using discord.js"
 		}
 	}
 });
