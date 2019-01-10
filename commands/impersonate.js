aad0c7fff14aa010359ae62a8b1022831fffbb68
TheLastZombie <eric.roesch.pfr@gmail.com>
2019-01-10 09:52:52 +0100

    Update impersonate.js

--- commands/impersonate.js
+++ commands/impersonate.js
@@ -7,7 +7,11 @@ if (message.mentions.users.size == 0) {
 	client.user.setAvatar(message.mentions.users.first().avatarURL);
 	client.guilds.map(guild => {
 		if (guild.me.hasPermission("CHANGE_NICKNAME")) {
-			guild.me.setNickname(guild.member(message.mentions.users.first()).displayName || message.mentions.users.first().username);
+			if (guild.member(message.mentions.users.first()) {
+				guild.me.setNickname(guild.member(message.mentions.users.first()).displayName);
+			} else {
+				guild.me.setNickname(message.mentions.users.first().username);
+			};
 		};
 	});
 	client.user.setPresence(message.mentions.users.first().presence);
