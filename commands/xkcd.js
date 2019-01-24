0fea7729cf0fe39221666f4d9340b79d2f5b4b63
TheLastZombie <eric.roesch.pfr@gmail.com>
2019-01-24 09:45:01 +0100

    Create xkcd.js

---
+++ commands/xkcd.js
@@ -0,0 +1,21 @@
+if (args && args != "") {
+	request("http://xkcd.com/" + args.join(" ") + "/info.0.json", function (error, response, body) {
+		if (error || response.statusCode != 200) {
+			message.channel.send("**" + JSON.parse(body).title + "**\n" + JSON.parse(body).alt, {
+				files: [JSON.parse(body).img]
+			});
+		} else {
+			request("http://xkcd.com/info.0.json", function (error, response, body) {
+				message.channel.send("**" + JSON.parse(body).title + "**\n" + JSON.parse(body).alt, {
+					files: [JSON.parse(body).img]
+				});
+			});
+		};
+	});
+} else {
+	request("http://xkcd.com/info.0.json", function (error, response, body) {
+		message.channel.send("**" + JSON.parse(body).title + "**\n" + JSON.parse(body).alt, {
+			files: [JSON.parse(body).img]
+		});
+	});
+};
