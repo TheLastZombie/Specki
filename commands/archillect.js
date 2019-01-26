1fcf9b6aa9016c6d95e9af0a7032b7bfbea8f233
TheLastZombie <eric.roesch.pfr@gmail.com>
2019-01-26 22:20:53 +0100

    Oof

--- commands/archillect.js
+++ commands/archillect.js
@@ -1,5 +1,5 @@
 request("https://archillect-api.now.sh/random", function (error, response, body) {
-	message.channel.send("Archillect ||| " + JSON.parse(body).id + " \|\|\| <" + JSON.parse(body).original + ">", {
+	message.channel.send("Archillect ||| " + JSON.parse(body).id + " \\|\\|\\| <" + JSON.parse(body).original + ">", {
 		files: [JSON.parse(body).imageSource]
 	});
 });
