2ca2421679b4c7a2b4f22b6f8c641732c2fb87ff
TheLastZombie <eric.roesch.pfr@gmail.com>
2019-03-05 15:43:59 +0100

    Update file.js

--- commands/file.js
+++ commands/file.js
@@ -1,17 +1,21 @@
-if (message.attachments.first()) {
-	request({
-		url: message.attachments.first().url,
-		encoding: null
-	}, function (error, response, body) {
-		message.channel.send("." + fileType(body).ext + " | " + fileType(body).mime);
-	});
-} else if (args && args != "") {
-	request({
-		url: args.join(" "),
-		encoding: null
-	}, function (error, response, body) {
-		message.channel.send("." + fileType(body).ext + " | " + fileType(body).mime);
-	});
-} else {
+try {
+	if (message.attachments.first()) {
+		request({
+			url: message.attachments.first().url,
+			encoding: null
+		}, function (error, response, body) {
+			message.channel.send("." + fileType(body).ext + " | " + fileType(body).mime);
+		});
+	} else if (args && args != "") {
+		request({
+			url: args.join(" "),
+			encoding: null
+		}, function (error, response, body) {
+			message.channel.send("." + fileType(body).ext + " | " + fileType(body).mime);
+		});
+	} else {
+		message.react("❎");
+	};
+} catch(e) {
 	message.react("❎");
 };
