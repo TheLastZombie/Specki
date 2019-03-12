d116fe1ccac03e8365833e57e0effd6f82b7bc3a
TheLastZombie <eric.roesch.pfr@gmail.com>
2019-03-12 08:21:34 +0000

    the api broke so fuck that

--- commands/archillect.js
+++ commands/archillect.js
@@ -1,5 +1,8 @@
-request("https://archillect-api.now.sh/random", function (error, response, body) {
-	message.channel.send("Archillect \\|\\|\\| " + JSON.parse(body).id + " \\|\\|\\| <" + JSON.parse(body).original + ">", {
-		files: [JSON.parse(body).imageSource]
+request("http://archillect.com/", function (error, response, body) {
+	var temp = Math.floor(Math.random() * body.match(/<div class="overlay"> \d+ <\/div>/).toString().slice(22, -7)) + 1;
+	request("http://archillect.com/" + temp, function (error, response, body) {
+		message.channel.send("Archillect \\|\\|\\| " + temp + " \\|\\|\\| <http://archillect.com/" + temp + ">", {
+			files: [body.match(/<img id="ii" src=.+ \/>/).toString().slice(17, -3)]
+		});
 	});
 });
