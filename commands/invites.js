fcb3212ed7a3f75cc1559ac288dfef415e89b6d2
TheLastZombie <eric.roesch.pfr@gmail.com>
2019-03-15 08:29:54 +0000

    Update invites.js

--- commands/invites.js
+++ commands/invites.js
@@ -1,18 +1,22 @@
 if (args && args != "") {
 	var collect = "";
-	client.guilds.find("name", args.join(" ")).fetchInvites().then(function(invites) {
-		if (invites.array().length > 0) {
-			collect += "**Invites for " + invites.array()[0].guild.name + "**\n";
-			for (index in invites.array()) {
-				collect += invites.array()[index].url + " (Channel: " + invites.array()[index].channel + ", Creator: " + invites.array()[index].inviter + ")\n";
+	try {
+		client.guilds.find("name", args.join(" ")).fetchInvites().then(function(invites) {
+			if (invites.array().length > 0) {
+				collect += "**Invites for " + invites.array()[0].guild.name + "**\n";
+				for (index in invites.array()) {
+					collect += invites.array()[index].url + " (Channel: " + invites.array()[index].channel + ", Creator: " + invites.array()[index].inviter + ")\n";
+				};
+				message.channel.send(collect);
+			} else {
+				client.guilds.find("name", args.join(" ")).channels.first().createInvite().then(function(invite) {
+					message.channel.send("**New Invite for " + invite.guild.name + "**\n" + invite.url + " (Channel: " + invite.channel + ", Creator: " + invite.inviter + ")\n");
+				});
 			};
-			message.channel.send(collect);
-		} else {
-			client.guilds.find("name", args.join(" ")).channels.first().createInvite().then(function(invite) {
-				message.channel.send("**New Invite for " + invite.guild.name + "**\n" + invite.url + " (Channel: " + invite.channel + ", Creator: " + invite.inviter + ")\n");
-			});
-		};
-	});
+		});
+	} catch(e) {
+		message.channel.send("Could neither get existing invites nor create a new one, likely due to insufficient permissions.");
+	};
 } else {
 	message.react("❎");
 };
