065e0ace3bf8e2a385d52a714a4464fcad56c0ca
TheLastZombie <eric.roesch.pfr@gmail.com>
2019-03-24 16:08:57 +0100

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -2,31 +2,12 @@ if (ownerIds.includes(message.author.id)) {
 	try {
 		var evout = eval(args.join(" "));
 		if (evout) {
-			message.channel.send("Output: \n```" + JSON.stringify(evout) + "```\n\nNote: Code is not running in a sandbox and thus console output not accessible.");
+			message.channel.send("Output: \n```" + JSON.stringify(evout) + "```");
 		};
+		message.react("✅");
 	} catch(err) {
 		message.channel.send("Error: \n```" + err.toString() + "```");
 	};
 } else {
-	var vmlog = "";
-	try {
-		vmout = new VM({
-			timeout: 5000,
-			sandbox: {
-				console: {
-					log: function(str) {
-						vmlog += JSON.stringify(str) + "\n";
-					}
-				}
-			}
-		}).run(args.join(" "));
-		var vmmsg = ((vmout != undefined) ? "Output: \n```" + JSON.stringify(vmout).replace(/`/g, "") + "```\n\n" : "") + ((vmlog != "") ? "Console: \n```" + vmlog.replace(/`/g, "") + "```\n\n" : "");
-		if (vmmsg) {
-			message.channel.send(vmmsg);
-		} else {
-			message.channel.send("Nothing returned! ¯\\_(ツ)_/¯");
-		};
-	} catch(err) {
-		message.channel.send("Error: \n```" + err.toString() + "```");
-	};
+	message.react("❎");
 };
