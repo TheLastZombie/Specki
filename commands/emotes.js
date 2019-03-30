79b5df295e86cae2ff2583e2edb6ccd2273bb512
TheLastZombie <eric.roesch.pfr@gmail.com>
2019-03-30 17:58:06 +0100

    Update emotes.js

--- commands/emotes.js
+++ commands/emotes.js
@@ -2,5 +2,5 @@ var temp = client.guilds.find("name", args.join(" "));
 if (temp == undefined) {
 	message.react("❎");
 } else {
-	message.channel.send("**Emotes in " + temp.name + "**\n" + message.guild.emojis.array().join(" "));
+	message.channel.send("**Emotes in " + temp.name + "**\n" + temp.emojis.array().join(" "));
 };
