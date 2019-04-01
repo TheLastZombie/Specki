111af952382457682f2c4ce802765b401afaef7b
TheLastZombie <eric.roesch.pfr@gmail.com>
2019-04-01 18:42:37 +0200

    hurensohn

--- commands/style.js
+++ commands/style.js
@@ -1,6 +1,6 @@
 var mode = args.shift();
 var temp = {};
-request("http://qaz.wtf/u/convert.cgi?text=" + args.join(" ") || "Enter your text!", function(error, response, body) {
+request("http://qaz.wtf/u/convert.cgi?text=" + (args.join(" ") || "Enter your text!"), function(error, response, body) {
     var $ = cheerio.load(body);
     $("tr").each(function() {
         temp[$(this).find("td:nth-child(1)").text().toLowerCase().replace(/\s/g, "-")] = $(this).find("td:nth-child(2)").text().trim();
