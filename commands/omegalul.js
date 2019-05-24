1ff60a21e294f7d17df9c49c9674e3e343077f11
Jerrynicki <38357030+Jerrynicki@users.noreply.github.com>
2019-05-24 20:45:33 +0200

    jetzt auch mit der id vom emote statt der nachricht

--- commands/omegalul.js
+++ commands/omegalul.js
@@ -1,9 +1,9 @@
 if (args && args != "") {
-	message.channel.send("**" + message.author.tag + ": **" + args.join(" ").replace(/o/gi, "<:omegalul:581551408252321834>"));
+	message.channel.send("**" + message.author.tag + ": **" + args.join(" ").replace(/o/gi, "<:omegalul:567236059772092426>"));
 } else {
 	message.channel.fetchMessages({
 		limit: 2
 	}).then(temp => {
-		message.channel.send("**" + message.author.tag + ": **" + temp.last().content.replace(/o/gi, "<:omegalul:581551408252321834>")));
+		message.channel.send("**" + message.author.tag + ": **" + temp.last().content.replace(/o/gi, "<:omegalul:567236059772092426>")));
 	});
 };
