e22fe93450ba01cfcc7b6875c38e45e638199dde
TheLastZombie <roesch.eric@protonmail.com>
2019-05-28 14:46:04 +0000

    Create recent.js

---
+++ commands/recent.js
@@ -0,0 +1,16 @@
+if (ownerIds.includes(message.author.id)) {
+	if (/\[.+\] \[.+\]/.test(args.join(" "))) {
+		var gld = args.join(" ").match(/\[.+\] \[/).toString().slice(1, -3);
+		var chn = args.join(" ").match(/\] \[.+\]/).toString().slice(3, -1);
+		client.guilds.find("name", gld).channels.find("name", chn).fetchMessages({
+			limit: 50
+		}).then(function(messages) {
+			var msg = "**Last messages in #" + chn + "**\n" + messages.map(x => "**" + x.author.tag + ":** " + x.content).reverse();
+			message.channel.send((msg.length > 2000 ? msg.substring(0, 1999) + "…" : msg)));
+		});
+	} else {
+		message.channel.send("Error: Invalid syntax or number of arguments!\nUsage: `" + process.env.PREFIX + command + " [Guild] [Channel]`");
+	};
+} else {
+	message.react("❎");
+};
