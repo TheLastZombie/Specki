7e0ce7f566bccabb162c80e85d62338471fb6058
TheLastZombie <roesch.eric@protonmail.com>
2019-07-18 09:59:53 +0000

    Add position to channel list

--- commands/channels.js
+++ commands/channels.js
@@ -2,5 +2,5 @@ var temp = client.guilds.find("name", args.join(" "));
 if (temp == undefined) {
 	message.react("❎");
 } else {
-	message.channel.send("**Channels in " + temp.name + "**\n" + temp.channels.map(chn => "[" + chn.type + "] " + chn.name).join("\n"));
+	message.channel.send("**Channels in " + temp.name + "**\n" + temp.channels.map(chn => "[" + chn.position + "] [" + chn.type + "] " + chn.name).join("\n"));
 };
