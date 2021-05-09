b360dcea39097cf19d026916d52ceaafccb1346d
TLZ <roesch.eric@protonmail.com>
2021-05-09 22:29:10 +0200

    Update dependencies

--- commands/file.js
+++ commands/file.js
@@ -3,15 +3,15 @@ try {
     request({
       url: message.attachments.first().url,
       encoding: null
-    }, function (error, response, body) {
-      message.channel.send('.' + fileType(body).ext + ' | ' + fileType(body).mime)
+    }, async function (error, response, body) {
+      message.channel.send('.' + await FileType.fromBuffer(body).ext + ' | ' + fileType(body).mime)
     })
   } else if (args && args != '') {
     request({
       url: args.join(' '),
       encoding: null
-    }, function (error, response, body) {
-      message.channel.send('.' + fileType(body).ext + ' | ' + fileType(body).mime)
+    }, async function (error, response, body) {
+      message.channel.send('.' + await FileType.fromBuffer(body).ext + ' | ' + fileType(body).mime)
     })
   } else {
     message.react('❎')
